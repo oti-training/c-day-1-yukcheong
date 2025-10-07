9cee1b75fedb1167eeccf9d54315da31bab671c9
yukcheong <tangyukcheong@live.com>
2025-10-07 09:11:55 +0800

    Done

--- q2_voltage/voltage.c
+++ q2_voltage/voltage.c
@@ -19,9 +19,9 @@
 int is_valid_voltage(float voltage) {
     // Check if voltage is outside the valid range (1.8V to 3.3V)
     if (voltage > 3.3f || voltage < 1.8f) {
-        return 1;  // Invalid
+        return 0;  // Invalid
     }
-    return 0; // Valid
+    return 1; // Valid
 }
 
 
